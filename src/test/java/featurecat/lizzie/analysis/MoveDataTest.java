917b4e04ee24a164ac2639eb0955deefb2a267b7
Wang Jing <jing_wang_from.CSP@lta.gov.sg>
2026-05-26 21:09:29 +0800

    Format MoveDataTest

--- src/test/java/featurecat/lizzie/analysis/MoveDataTest.java
+++ src/test/java/featurecat/lizzie/analysis/MoveDataTest.java
@@ -16,6 +16,7 @@ public class MoveDataTest {
   public static void setUpClass() throws IOException {
     Lizzie.config = new Config();
   }
+
   @Test
   public void testFromInfoLine() {
     String info = "move R5 visits 38 winrate 5404 order 0 pv R5 Q5 R6 S4 Q10 C3 D3 C4 C6 C5 D5";
